24ed8c524c705fe21bab87392bbd9ca6bf7f65de
takoyaki-3 <mail@takoyaki3.com>
2024-04-07 17:02:13 +0900

    返却可能数を表示する仕様に変更

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // 地図の初期化
-var mymap = L.map('mapid').setView([35.681236, 139.767125], 13); // 東京駅の座標を初期値とする
+var mymap = L.map('mapid').setView([35.681236, 139.767125], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,40 +10,56 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var markers = L.markerClusterGroup();
 
 // 自転車ステーションのピン画像URL
-var bikePinUrl = './bike_stop_icon.png'; // ここにアップロードした自転車ピンのURLを入力してください
+var bikePinUrl = './bike_stop_icon.png'; 
 
 // ピンのアイコンを定義
 var bikeIcon = L.icon({
     iconUrl: bikePinUrl,
-    iconSize: [38, 38], // アイコンのサイズ
-    iconAnchor: [22, 94], // アイコンの"釘"の部分
-    popupAnchor: [-3, -76] // ポップアップが開く位置
+    iconSize: [38, 38], 
+    iconAnchor: [19, 38], 
+    popupAnchor: [0, -38] 
 });
 
-// GBFSのJSONデータを取得し、処理する関数
-function fetchAndProcessGBFSData() {
-    fetch('https://api-public.odpt.org/api/v4/gbfs/hellocycling/station_information.json')
+// ステーションの情報を取得する
+function fetchStationInformation() {
+    return fetch('https://api-public.odpt.org/api/v4/gbfs/hellocycling/station_information.json')
         .then(response => response.json())
-        .then(data => {
-            const stations = data.data.stations; // 仮定のパス: 実際のデータ構造に合わせて調整してください
-            stations.forEach(station => {
-                var marker = L.marker([station.lat, station.lon], { icon: bikeIcon })
-                    .bindPopup(`<b>${station.name}</b><br>利用可能な自転車: ${station.num_bikes_available}`);
-                markers.addLayer(marker);
-            });
-            mymap.addLayer(markers);
-        })
-        .catch(error => console.error('Error loading GBFS data:', error));
+        .then(data => data.data.stations);
 }
+
+// ステーションのステータスを取得する
+function fetchStationStatus() {
+    return fetch('https://api-public.odpt.org/api/v4/gbfs/hellocycling/station_status.json')
+        .then(response => response.json())
+        .then(data => data.data.stations);
+}
+
+// ステーションの情報とステータスをマージする
+function addStationsToMap(stationsInfo, stationsStatus) {
+    const statusIndex = stationsStatus.reduce((acc, station) => {
+        acc[station.station_id] = station;
+        return acc;
+    }, {});
+
+    stationsInfo.forEach(stationInfo => {
+        const stationStatus = statusIndex[stationInfo.station_id];
+        var popupContent = `<b>${stationInfo.name}</b><br>` +
+                           `利用可能な自転車: ${stationStatus.num_bikes_available}<br>` +
+                           `返却可能数: ${stationStatus.num_docks_available}`;
+        var marker = L.marker([stationInfo.lat, stationInfo.lon], { icon: bikeIcon })
+            .bindPopup(popupContent);
+        markers.addLayer(marker);
+    });
+
+    mymap.addLayer(markers);
+}
+
+// データを取得してマップに追加する
+Promise.all([fetchStationInformation(), fetchStationStatus()])
+    .then(([stationsInfo, stationsStatus]) => addStationsToMap(stationsInfo, stationsStatus))
+    .catch(error => console.error('Error loading GBFS data:', error));
 
 // 地図のズームレベルが変わったときのイベントハンドラを追加
 mymap.on('zoomend', function() {
-    var currentZoom = mymap.getZoom();
-    
-    // 地図のズームレベルに応じた制御が必要な場合はここにコードを追加
-    // このサンプルでは、ズームレベルによる表示のオンオフ切り替えは行っていませんが、
-    // 必要に応じて特定のズームレベルで特定のアクションを行うことができます。
+    // ズームレベルに基づくアクションをここに追加することができます
 });
-
-// GBFSデータを取得して処理する関数を実行
-fetchAndProcessGBFSData();
